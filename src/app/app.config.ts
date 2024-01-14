681f162a144bb51add83815ba99b9deef85a5f5a
Daniel Sisovic <d.sisovic777@gmail.com>
2024-01-14 19:10:47 +0100

    Add route hashing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { routes } from './app.routes';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withViewTransitions())]
+  providers: [provideRouter(routes, withViewTransitions(), withHashLocation())]
 };
